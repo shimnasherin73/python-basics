8a2053085c84455e7c296e982a8f492ccf1c5145
shimnasherin73 <shimnashahulpt@gmail.com>
2026-04-21 13:08:10 +0530

    cs

--- conditional statemnt.py
+++ conditional statemnt.py
@@ -1,11 +1,27 @@
+#condtion statmnt:
 x=1
-if x>5:
+if x>5: #needed to be true 
     print("x is greater than 5")
-else:
+else: #if the (if) is false 
     print("x is not grater than 5")    
     
-x=10
+x=7
 if x%2==0:
-    print("x is even number")
+    print(f"{x} is even number") #f=to get number in output
 else:
-    print("x is odd number")
+    print(f"{x} is odd number")
+     
+x=45
+if x%3==0:
+    print(f"{x} is multiple of 3")
+else:
+    print(f"{x} is not a multple of 3")
+    
+x=7#if there is 3 conditions 
+if x>10:
+    print("x is greater than 10")
+elif x<5:
+    print("x is less than 5")
+else:
+    print("x is between 5&10") 
+    
